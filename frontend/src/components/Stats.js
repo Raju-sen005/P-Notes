21b74061a8200be6567511638c1ad4d5bedf0ab3
Raju_0005 <rajugbp2006@gmail.com>
2025-06-06 11:29:20 +0530

    Update Stats.js

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import CountUp from 'react-countup';
-import axios from 'axios';
+import API from "../api/axios";
 
 const Stats = () => {
   const [stats, setStats] = useState({
