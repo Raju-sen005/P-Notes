b6be1af04d4cc0313b8e3566e064b78aee2d9a11
Raju_0005 <rajugbp2006@gmail.com>
2025-06-22 21:20:41 +0530

    Update ArticleList.js

--- frontend/src/pages/ArticleList.js
+++ frontend/src/pages/ArticleList.js
@@ -57,7 +57,7 @@ const ArticleList = () => {
         <p className="text-center text-gray-600">No articles found.</p>
       ) : (
         <motion.div
-          className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+          className="a-container container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
           initial="hidden"
           animate="visible"
           variants={{
