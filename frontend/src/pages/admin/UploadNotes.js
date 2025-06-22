de4c2e5b1daef0f1498cb7b9d3895228e9d52338
Raju_0005 <rajugbp2006@gmail.com>
2025-06-22 10:40:34 +0530

    Update UploadNotes.js

--- frontend/src/pages/admin/UploadNotes.js
+++ frontend/src/pages/admin/UploadNotes.js
@@ -22,7 +22,7 @@ const UploadNotes = () => {
     e.preventDefault();
     setMessage("");
     try {
-      await API.post("https://p-notes-backend.onrender.com/notes", formData);
+      await API.post("/notes", formData);
       setMessage("✅ Note uploaded successfully!");
       setFormData({
         title: "",
