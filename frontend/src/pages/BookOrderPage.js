117432dc647f0579a6a3594e75a8d968bbc1a660
Raju-sen005 <rajugbp2006@gmail.com>
2025-06-24 08:06:48 +0530

    Add BookOrderPage component

---
+++ frontend/src/pages/BookOrderPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import BookOrderForm from "../pages/BookOrderForm";
+import axios from "axios";
+
+const BookOrderPage = () => {
+  const handleOrderSubmit = async (orderData) => {
+    const token = localStorage.getItem("token");
+    await axios.post("/api/orders", orderData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+  return <BookOrderForm onSubmit={handleOrderSubmit} />;
+};
+
+export default BookOrderPage;
